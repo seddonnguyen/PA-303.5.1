457ac60c4f1860b8de9111c1da07be9b21849eeb
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-03-18 22:17:03 -0600

    Add project files

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,13 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Task 1: Print a Multiplication Table
+        System.out.printf("%16sMultiplication Table%n", " ");
+        int size = 12;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                System.out.printf("%4d", i * j);
+            }
+            System.out.println();
+        }
+    }
+}
